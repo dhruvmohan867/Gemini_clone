2d5dca0e35de70a53b9fc12828ab020d32ee3323
dhruvmohan867 <127665916+dhruvmohan867@users.noreply.github.com>
2025-01-07 19:50:54 +0530

    Update gemini.js

--- src/lib/gemini.js
+++ src/lib/gemini.js
@@ -8,7 +8,7 @@ import {
   } from "@google/generative-ai";
   
   const MODEL_NAME = "gemini-1.5-flash";
-  const API_KEY = "AIzaSyDZZdEr3bHPHlbTiPjN0wD0DBg_VxtV4Eg";
+  const API_KEY = "AIzaSyCHcnvbhfQuTeEGjYEw97w6FqJEgzpW7Fg";
   
   async function runChat(prompt) {
     const genAI = new GoogleGenerativeAI(API_KEY);
